62eeab4887e95379756a193e9f5be98cc4b8b844
moosspanic <unrest50@gmail.com>
2021-10-01 14:22:59 +0500

    windows1

---
+++ app/src/main/java/ru/unit6/course/android/retrofit/MainActivity.kt
@@ -0,0 +1,41 @@
+package ru.unit6.course.android.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.system.Os.bind
+import android.view.View
+import android.widget.TextView
+import ru.unit6.course.android.retrofit.ui.main.MainAdapter
+import ru.unit6.course.android.retrofit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+
+            val un: TextView
+            try {
+                println("########## un наполнение")
+                un = MainAdapter.DataViewHolder.
+                    findViewById<TextView>(R.id.textViewUserName)
+                try {
+                    un.setOnClickListener(View.OnClickListener {
+                        println("########## нажал на имя")
+                    })
+                } catch (e: Exception) {
+                    println("##########         Exception setOnClickListener")
+                }
+
+            } catch (e: Exception) {
+                println("##########         Exception findById")
+            }
+        }
+        //recyclerView.adapter.getItemCount()
+
+    }
+}
---
+++ app/src/main/java/ru/unit6/course/android/retrofit/ui/main/MainFragment.kt
@@ -0,0 +1,89 @@
+package ru.unit6.course.android.retrofit.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import ru.unit6.course.android.retrofit.R
+import ru.unit6.course.android.retrofit.data.model.User
+import ru.unit6.course.android.retrofit.utils.Status
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+
+        view.apply {
+            recyclerView = findViewById(R.id.recyclerView)
+            progressBar = findViewById(R.id.progressBar)
+        }
+
+        return view
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        setupUI()
+        setupObservers()
+    }
+
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = MainAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupObservers() {
+        viewModel.getUsers().observe(viewLifecycleOwner) { resource ->
+            when (resource.status) {
+                Status.SUCCESS -> {
+                    recyclerView.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    resource.data?.let { users -> adapter.addUsers(users) }
+                    println("######### Observers SUCCESS")
+                }
+                Status.ERROR -> {
+                    recyclerView.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(context, resource.message, Toast.LENGTH_LONG).show()
+                    println("######### Observers ERROR")
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
+                    println("######### Observers LOADINGg")
+                }
+            }
+        }
+    }
+}
